3cff9f1c65fa069babe01234e2750ba639fa21c6
贺庆 <heqing@heqingdeMacBook-Pro.local>
2021-07-29 01:50:46 +0100

    Forth commit for chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rango.models import Category
 from django.http import HttpResponse
+from rango.models import Page
+# Create your views here.
+
 def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    #. The - in -likes denotes that we would like them in descending order (if we removed the - then the results would be returned in ascending order).
+    context_dict = {}
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
+
+#show the detail of each category
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    
+    return render(request, 'rango/category.html', context=context_dict)
